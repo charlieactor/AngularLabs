26382d84c4f442f20539ad86d68feabe6358c122
Charlie Actor <Charlie@Charless-MacBook-Pro.local>
2017-08-25 09:12:27 -0600

    angular labs

--- ngHandsOn/WebContent/app/appModule/productList.component.js
+++ ngHandsOn/WebContent/app/appModule/productList.component.js
@@ -1,4 +1,25 @@
 angular.module('appModule')
 			  .component('productList', {
-				  templateUrl : 'app/appModule/productList.component.html'
+				  templateUrl : 'app/appModule/productList.component.html',
+				  controller  : function(productService) {
+					  //Step 1 - label vm as the component (also the controller?)
+					  var vm = this;
+					  
+					  vm.products = productService.index();
+					  
+					  vm.getNumItems = function() {
+						return vm.products.length;  
+					  };
+					  
+					  vm.log = function(string) {
+						  console.log(string);
+					  };
+					  
+					  vm.addItem = function(prod) {
+						  productService.create(prod);
+					  }
+					  
+					  
+				  },
+			  	 controllerAs : "vm"
 			  });
